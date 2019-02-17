b1a652c0c77efe0dacfbc11913f5d5b9df50fbbd
amoghbhat007 <47709509+amoghbhat007@users.noreply.github.com>
2019-02-17 19:42:14 +0530

    Add files via upload

---
+++ map/data/dialogue.js
@@ -0,0 +1,26 @@
+var dialogue=new Array()
+
+dialogue[0]="Switch off NearBy first!"
+dialogue[1]='<img src=nearbyoff.png height=auto width=400px/>'
+dialogue[2]="Showing all location within 1 Kilometer radius"
+dialogue[3]=" location found"
+dialogue[4]="<br>__________________________________________________<br><br>"
+dialogue[5]='style="float:left; margin-right:5px; margin-bottom:5px; border:solid #777 1px; padding:2px"'
+dialogue[6]=" not found, try searching something else!"
+dialogue[7]="Welcome to maps!!!!"
+dialogue[8]="Parks and playgrounds"
+dialogue[9]="Toystores"
+dialogue[10]="Schools"
+dialogue[11]="pediatricians"
+dialogue[12]="about"
+dialogue[13]="You're somewhere around here!"
+dialogue[14]=",an easy way to navigate around!!"
+dialogue[15]="weather today"
+dialogue[16]="finding your location"
+dialogue[17]="day cares"
+dialogue[18]='<img src=nearby.png height=auto width=400px/>' 
+dialogue[19]="Tuning in to cartoon network!!"
+dialogue[20]='<img src=notfound.jpg height=auto width=400px/>'
+dialogue[21]="Oh!!, there you are"
+dialogue[22]='<br><br><button type="button" onclick="showDetails()">back</button>'
+dialogue[23]=" locations found"
